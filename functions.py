77f3b29020466a6cad0859758a8890ec82d6db9a
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-04 00:13:46 +0530

    completed basic implementations

--- functions.py
+++ functions.py
@@ -1,28 +1,51 @@
 import random as rd
 
-def get_ll(l):
+def get_ll(gen):
     with open("Lletters.txt", "r") as file:
         mems = file.readlines()
         mems = rd.choice(mems)
         write_(mems.strip())
+        gen+=1
+        return gen
 
-def get_ul(l):
+def get_ul(gen):
     with open("Uletters.txt", "r") as file:
         mems = file.readlines()
         mems = rd.choice(mems)
         write_(mems.strip())
+        gen += 1
+        return gen
 
-def get_n(l):
+def get_n(gen):
     with open("numbers.txt", "r") as file:
         mems = file.readlines()
         mems = rd.choice(mems)
         write_(mems.strip())
+        gen += 1
+        return gen
 
-def get_ss(l):
+def get_ss(gen):
     with open("special_characters.txt", "r") as file:
         mems = file.readlines()
         mems = rd.choice(mems)
         write_(mems.strip())
+        gen += 1
+        return gen
+
+def randomizer(selected,gen):
+    selected = rd.choice(selected)
+    if selected == "uc":
+        gen = get_ul(gen)
+        return gen
+    if selected == "lc":
+        gen = get_ll(gen)
+        return gen
+    if selected == "n":
+        gen = get_n(gen)
+        return gen
+    if selected == "sc":
+        gen = get_ss(gen)
+        return gen
 
 def write_(args):
     with open("password.txt", "a") as file:
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import random as rd
 import functions
 
 st.set_page_config(layout='wide')
@@ -16,15 +15,32 @@ if l:
     n_checkbox = st.checkbox("Numbers", key = "nc")
     ss_checkbox = st.checkbox("Special Characters", key ="ss")
 
+    gen = 0
+    check = 0
+    selected = []
+
     if st.button("GENERATE", type = "primary"):
         if u_checkbox:
-            functions.get_ul(l)
+            check += 1
+            selected.append("uc")
+            gen = functions.get_ul(gen)
         if l_checkbox:
-            functions.get_ll(l)
+            check += 1
+            selected.append("lc")
+            gen = functions.get_ll(gen)
         if n_checkbox:
-            functions.get_n(l)
+            check += 1
+            selected.append("n")
+            gen = functions.get_n(gen)
         if ss_checkbox:
-            functions.get_ss(l)
+            check += 1
+            selected.append("sc")
+            gen = functions.get_ss(gen)
+
+        if check == gen:
+            while gen < l:
+                    gen = functions.randomizer(selected,gen)
+
         dis = functions.display()
         st.write(dis)
 
