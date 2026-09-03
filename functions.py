efef12ebd9ab16d1d8dcf0fd5ed588545e9aaef8
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-03 22:58:08 +0530

    updated

--- functions.py
+++ functions.py
@@ -4,25 +4,25 @@ def get_ll(l):
     with open("Lletters.txt", "r") as file:
         mems = file.readlines()
         mems = rd.choice(mems)
-        write_(mems)
+        write_(mems.strip())
 
 def get_ul(l):
     with open("Uletters.txt", "r") as file:
         mems = file.readlines()
         mems = rd.choice(mems)
-        write_(mems)
+        write_(mems.strip())
 
 def get_n(l):
     with open("numbers.txt", "r") as file:
         mems = file.readlines()
         mems = rd.choice(mems)
-        write_(mems)
+        write_(mems.strip())
 
 def get_ss(l):
     with open("special_characters.txt", "r") as file:
         mems = file.readlines()
         mems = rd.choice(mems)
-        write_(mems)
+        write_(mems.strip())
 
 def write_(args):
     with open("password.txt", "a") as file:
